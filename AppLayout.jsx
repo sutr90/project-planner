4faa0d81282ab34e22eb944eb69030e6e4811d88
Vojtěch Frič <you@example.com>
2020-02-08 20:59:48 +0100

    added tree visualization

--- AppLayout.jsx
+++ AppLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Row, Col } from 'antd';
 import Table from './Table'
-import VisNetwork from './VisNetwork';
+import CenteredTree from './CenteredTree';
 
 export default function AppLayout() {
     return <Row>
         <Col sm={12}><Table /></Col>
-        <Col sm={12}><VisNetwork /></Col>
+        <Col sm={12}><CenteredTree /></Col>
     </Row>
 }
