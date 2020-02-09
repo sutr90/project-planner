f90fa6ba75b864fe495ef0ea5cddee8abcbad890
Vojtěch Frič <you@example.com>
2020-02-09 12:00:57 +0100

    fixed attribute rendering

--- CenteredTree.jsx
+++ CenteredTree.jsx
@@ -1,122 +1,122 @@
-import React from "react";
-import Tree, {treeUtil} from "react-d3-tree";
-
+import React, { useState, useCallback } from "react";
+import Tree, { treeUtil } from "react-d3-tree";
+import { connect } from 'react-redux'
 const debugData = [{
-    "parent": "CSVNode1",
-    "child": "CSVNode2",
-    "FlatJSON Attribute A": "22",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode1",
-    "child": "CSVNode3",
-    "FlatJSON Attribute A": "23",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode4",
-    "FlatJSON Attribute A": "1",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode5",
-    "FlatJSON Attribute A": "2",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode6",
-    "FlatJSON Attribute A": "3",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode7",
-    "FlatJSON Attribute A": "4",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode8",
-    "FlatJSON Attribute A": "5",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode9",
-    "FlatJSON Attribute A": "6",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode2",
-    "child": "CSVNode10",
-    "FlatJSON Attribute A": "7",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode4",
-    "child": "CSVNode11",
-    "FlatJSON Attribute A": "8",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode7",
-    "child": "CSVNode12",
-    "FlatJSON Attribute A": "9",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode9",
-    "child": "CSVNode13",
-    "FlatJSON Attribute A": "10",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode10",
-    "child": "CSVNode14",
-    "FlatJSON Attribute A": "11",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode11",
-    "child": "CSVNode15",
-    "FlatJSON Attribute A": "12",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode3",
-    "child": "CSVNode16",
-    "FlatJSON Attribute A": "13",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode16",
-    "child": "CSVNode17",
-    "FlatJSON Attribute A": "14",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode16",
-    "child": "CSVNode18",
-    "FlatJSON Attribute A": "15",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode16",
-    "child": "CSVNode19",
-    "FlatJSON Attribute A": "16",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode16",
-    "child": "CSVNode20",
-    "FlatJSON Attribute A": "17",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode17",
-    "child": "CSVNode21",
-    "FlatJSON Attribute A": "18",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode18",
-    "child": "CSVNode22",
-    "FlatJSON Attribute A": "19",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode19",
-    "child": "CSVNode23",
-    "FlatJSON Attribute A": "20",
-    "FlatJSON Attribute B": "someValue"
-  }, {
-    "parent": "CSVNode20",
-    "child": "CSVNode24",
-    "FlatJSON Attribute A": "21",
-    "FlatJSON Attribute B": "someValue"
-  }];
+  "parent": "CSVNode1",
+  "child": "CSVNode2",
+  "attA": "22",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode1",
+  "child": "CSVNode3",
+  "attA": "23",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode4",
+  "attA": "1",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode5",
+  "attA": "2",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode6",
+  "attA": "3",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode7",
+  "attA": "4",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode8",
+  "attA": "5",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode9",
+  "attA": "6",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode2",
+  "child": "CSVNode10",
+  "attA": "7",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode4",
+  "child": "CSVNode11",
+  "attA": "8",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode7",
+  "child": "CSVNode12",
+  "attA": "9",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode9",
+  "child": "CSVNode13",
+  "attA": "10",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode10",
+  "child": "CSVNode14",
+  "attA": "11",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode11",
+  "child": "CSVNode15",
+  "attA": "12",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode3",
+  "child": "CSVNode16",
+  "attA": "13",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode16",
+  "child": "CSVNode17",
+  "attA": "14",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode16",
+  "child": "CSVNode18",
+  "attA": "15",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode16",
+  "child": "CSVNode19",
+  "attA": "16",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode16",
+  "child": "CSVNode20",
+  "attA": "17",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode17",
+  "child": "CSVNode21",
+  "attA": "18",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode18",
+  "child": "CSVNode22",
+  "attA": "19",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode19",
+  "child": "CSVNode23",
+  "attA": "20",
+  "attB": "someValue"
+}, {
+  "parent": "CSVNode20",
+  "child": "CSVNode24",
+  "attA": "21",
+  "attB": "someValue"
+}];
 
 const containerStyles = {
   width: '100%',
@@ -124,28 +124,102 @@ const containerStyles = {
   overflow: 'hidden'
 }
 
-export default class CenteredTree extends React.PureComponent {
-  state = {}
 
-  componentDidMount() {
-    const dimensions = this.treeContainer.getBoundingClientRect();
-    this.setState({
-      translate: {
-        x: dimensions.width / 2,
-        y: dimensions.height / 2
+
+function _transformToHierarchy(links, attributeFields) {
+  const nodesByName = {};
+
+  const assignNode = name => {
+    if (!nodesByName[name]) {
+      nodesByName[name] = { name };
+    }
+    return nodesByName[name];
+  };
+
+  const assignNodeWithAttributes = (name, attributes) => {
+    if (!nodesByName[name]) {
+      nodesByName[name] = {
+        name,
+        attributes,
+      };
+    }
+    return nodesByName[name];
+  };
+
+  // Create nodes for each unique source and target.
+  links.forEach(link => {
+    // if `attributeFields` is defined, create/overwrite current `link.attributes`
+    if (attributeFields) {
+      const customAttributes = {};
+      attributeFields.forEach(field => {
+        customAttributes[field] = link[field];
+      });
+      link.attributes = customAttributes;
+    }
+
+    link.source = assignNode(link.parent);
+    link.target = assignNodeWithAttributes(link.child, link.attributes);
+    const parent = link.source;
+    const child = link.target;
+
+    parent.id = Math.random();
+    child.id = Math.random();
+    child.parent = parent.name || null;
+
+    parent._collapsed = child._collapsed = false; // eslint-disable-line
+    // NOTE We assign to a custom `_children` field instead of D3's reserved
+    // `children` to avoid update anomalies when collapsing/re-expanding nodes.
+    parent._children ? parent._children.push(child) : (parent._children = [child]);
+  });
+
+  // Extract & return the root node
+  const rootLinks = links.filter(link => !link.source.parent);
+  return [rootLinks[0].source];
+}
+
+
+
+
+function useClientRect() {
+  const [rect, setRect] = useState(null);
+  const ref = useCallback(node => {
+    if (node !== null) {
+      setRect(node.getBoundingClientRect());
+    }
+  }, []);
+  return [rect, ref];
+}
+
+const CenteredTree = props => {
+  const [rect, ref] = useClientRect();
+
+  const getTranslate = () => {
+    if (rect) {
+      return {
+        x: rect.width / 2,
+        y: rect.height / 2
       }
-    });
+    }
+
+    return { x: 0, y: 0 };
   }
 
-  render() {
-    return (
-      <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
-        <Tree 
-          data={treeUtil.generateHierarchy(debugData)} 
-          translate={this.state.translate} 
-          orientation={'vertical'}
-        />
-      </div>
-    );
+  const data = _transformToHierarchy(debugData, ["attA", "attB"]);
+  console.log(data)
+  return <div style={containerStyles} ref={ref}>
+    <Tree
+      data={data}
+      translate={getTranslate()}
+      orientation={'vertical'}
+    />
+  </div>
+}
+
+
+const mapStateToProps = state => {
+  return {
+    rows: state.rows,
   }
 }
+
+export default connect(mapStateToProps)(CenteredTree);
