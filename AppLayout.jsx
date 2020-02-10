ec5650b0bdbf7e2cc8b7501328e1661592a412a0
Vojtěch Frič <you@example.com>
2020-02-11 00:16:52 +0100

    added cytoscape layout

--- AppLayout.jsx
+++ AppLayout.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Row, Col } from 'antd';
 import Table from './Table'
 import CenteredTree from './CenteredTree';
+import CytoscapeGraph from './CytoscapeGraph';
 
 export default function AppLayout() {
     return <Row>
         <Col sm={12}><Table /></Col>
-        <Col sm={12}><CenteredTree /></Col>
+        <Col sm={12}><CytoscapeGraph /></Col>
     </Row>
 }
---
+++ CytoscapeGraph.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import CytoscapeComponent from 'react-cytoscapejs';
+import { connect } from 'react-redux'
+
+const cytograph = (props) => {
+    const elements = [
+        { data: { id: 'one', label: 'Node 1' } },
+        { data: { id: 'two', label: 'Node 2' } },
+        { data: { source: 'one', target: 'two' } }
+    ];
+
+    const rowsToHierarchy = () => {
+        const rowToNode = row => {
+            return [{ data: { id: row.id, label: row.name } }, ...rowToEdge(row)];
+        };
+
+        const rowToEdge = row => {
+            return row.deps.map(dep => { return { data: { source: dep, target: row.id } } });
+        };
+
+        const { rows } = props;
+
+        return [...rows.map(row => rowToNode(row))].flat();
+    };
+
+
+    if (props.rows && props.rows.length <= 1) {
+        return <Empty />;
+    }
+
+    const handleCy = (cy) => {
+        cy.on('add remove', () => {
+            cy.layout({
+                name: 'breadthfirst',
+                directed: true,
+                padding: 10
+            }).run();
+        });
+    }
+
+    console.log(rowsToHierarchy());
+
+    return <CytoscapeComponent elements={[...rowsToHierarchy()]} style={{
+        width: '100%',
+        height: '90vh',
+        overflow: 'hidden'
+    }}
+        layout={{
+            name: 'breadthfirst',
+            directed: true,
+            padding: 10
+        }}
+
+        stylesheet={[{
+            selector: 'edge',
+            style: {
+                'curve-style': 'bezier',
+                'width': 6,
+                'target-arrow-shape': 'triangle',
+                'line-color': '#ffaaaa',
+                'target-arrow-color': '#ffaaaa'
+            }
+        }]}
+
+        cy={handleCy}
+
+    />;
+}
+
+const mapStateToProps = state => {
+    return {
+        rows: state.rows,
+    }
+}
+
+export default connect(mapStateToProps)(cytograph);
