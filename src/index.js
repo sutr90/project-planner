33964afd2b7e47fb37643a02a36837e37be50a13
Vojtěch Frič <you@example.com>
2020-02-13 21:03:30 +0100

    updated build process

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App'
 import 'antd/dist/antd.css';
+import { AppContainer } from 'react-hot-loader'
 
 const mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(<AppContainer><App /></AppContainer>, mountNode);
